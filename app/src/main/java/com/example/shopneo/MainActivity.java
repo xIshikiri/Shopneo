79d57401126c91b2ac03abce31900723e88a3afc
xIshikiri <bartek_g@poczta.fm>
2022-12-11 00:19:05 +0100

    Database product data import.

--- app/src/main/java/com/example/shopneo/MainActivity.java
+++ app/src/main/java/com/example/shopneo/MainActivity.java
@@ -2,13 +2,21 @@ package com.example.shopneo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.TypedArray;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     Shopneo.ShopneoDBHelper dbHelper;
     SQLiteDatabase db;
+    Button button;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +25,15 @@ public class MainActivity extends AppCompatActivity {
 
         dbHelper = new Shopneo.ShopneoDBHelper(this);
         db = dbHelper.getWritableDatabase();
+
+        button = findViewById(R.id.button);
+        imageView = findViewById(R.id.example_photo);
+
+        button.setOnClickListener(l -> {
+            TypedArray images = getResources().obtainTypedArray(R.array.article_photos);
+            imageView.setImageResource(images.getResourceId(new Random().nextInt(3), -1));
+            images.recycle();
+        });
 
     }
 }
