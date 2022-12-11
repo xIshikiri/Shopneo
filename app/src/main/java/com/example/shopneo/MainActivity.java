0971b75c7a7e0bb8be3aecbfe6c99e1c4b8f3111
xIshikiri <bartek_g@poczta.fm>
2022-12-11 01:34:41 +0100

    Working test user login.

--- app/src/main/java/com/example/shopneo/MainActivity.java
+++ app/src/main/java/com/example/shopneo/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.shopneo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.res.TypedArray;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.provider.BaseColumns;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.util.Random;
 
@@ -17,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
     SQLiteDatabase db;
     Button button;
     ImageView imageView;
+    EditText email;
+    EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +34,21 @@ public class MainActivity extends AppCompatActivity {
 
         button = findViewById(R.id.button);
         imageView = findViewById(R.id.example_photo);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
 
         button.setOnClickListener(l -> {
-            TypedArray images = getResources().obtainTypedArray(R.array.article_photos);
-            imageView.setImageResource(images.getResourceId(new Random().nextInt(3), -1));
-            images.recycle();
+            String e = email.getText().toString();
+            String p = password.getText().toString();
+            Log.i("db", e + " " + p);
+            Cursor cursor = db.query("Account", new String[]{"email", "password"}, "email=? AND password=?", new String[]{e, p}, null, null, null);
+            Log.i("db", String.valueOf(cursor.getCount()));
+            if (cursor.getCount() == 1){
+                Toast.makeText(this, "Correct", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Incorrect", Toast.LENGTH_SHORT).show();
+            }
+            cursor.close();
         });
 
     }
