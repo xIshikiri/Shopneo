f24cfef59be829a98f38ed3e12ccd3a103d6510d
edunni <94446799+edunni@users.noreply.github.com>
2023-01-04 22:14:12 +0100

    added ItemShowcaseFragment

---
+++ app/src/main/java/com/example/shopneo/main/fragment/ItemShowcaseFragment.java
@@ -0,0 +1,47 @@
+package com.example.shopneo.main.fragment;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.example.shopneo.R;
+
+public class ItemShowcaseFragment extends Fragment {
+
+    public ItemShowcaseFragment() {
+        // Required empty public constructor
+    }
+
+    Spinner spinner;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_item_showcase, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        spinner = view.findViewById(R.id.itemShowCaseQuantity);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.article_sizes, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+}
